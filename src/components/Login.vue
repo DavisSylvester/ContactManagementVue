b61fe9d4901d6e8b015498f79902d5eed2c896b1
Davis Sylvester III <davis@sylvesterllc.com>
2019-01-20 16:26:18 -0600

    Added single property to login for testing

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,41 +1,42 @@
-
-
-
 <template>
-<div id="login-container">
-    <button class="btn-login">Login</button>
-</div>    
+  <div id="login-container">
+    <button v-if="isLoggedIn" class="btn-login">Login</button>
+
+  </div>
 </template>
 
 <script>
-import '../less/variables.less';
-
 export default {
-    
-}
+  data: function() {
+    return {
+      isLoggedIn: true
+    };
+  }
+};
 </script>
 
 
 <style lang="less" scoped>
+@import "../less/variables.less";
 
 .btn-login {
-    justify-self: flex-end;
-    align-self: center;
-    height: @headerHeight - 2;
-    padding: 0px 30px;
-    font-family: 'Open Sans';
-    font-size: 18px;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    border-top-left-radius: 2px;
-    border-top-right-radius: 2px;
-    margin-right: 1%;
-    background: @darkRedBorderColor;
-    color: #fff;
-    border-top-color: @darkRedBorderColor;
-    border-right-color: @darkRedBorderColor;
-    border-left-color: @darkRedBorderColor;
-    border-bottom-color:darken(@darkRedBorderColor, 9%);
-    border-bottom-width: 2px;
+  justify-self: flex-end;
+  align-self: center;
+  // height: @headerHeight - 2;
+  padding: 3px 30px;
+  font-family: "Open Sans";
+  font-size: 18px;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  border-top-left-radius: 2px;
+  border-top-right-radius: 2px;
+  margin-right: 1%;
+  color: #fff;
+  background: @darkRedBorderColor;
+  border-top-color: @darkRedBorderColor;
+  border-right-color: @darkRedBorderColor;
+  border-left-color: @darkRedBorderColor;
+  border-bottom-color: darken(@darkRedBorderColor, 9%);
+  border-bottom-width: 2px;
 }
 </style>
